2b71c965469e550f7998c96a9dc6a95e2442bd0f
abhijeetjuneja <abhijeetjuneja2014@gmail.com>
2020-01-01 19:33:40 +0530

    Added routes and Error handler

---
+++ modules/errorHandler.js
@@ -0,0 +1,9 @@
+class errorHandler extends Error {
+    constructor(status, message) {
+        super();
+        this.status = status;
+        this.message = message;
+    }
+}
+
+module.exports = errorHandler
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express')
 const path = require('path')
 const bodyParser = require('body-parser')
 const cors = require('cors')
+const result = require('./routes/result')
 
 //Import mongoose module
 const mongoose = require('./modules/mongoose')
@@ -12,13 +13,36 @@ app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(cors())
 
-//Port selection
-var port = process.env.PORT || 4000
+//Result router
+app.use('/api/results/', result)
+
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+    var err = new Error('404 Not Found')
+    err.status = 404
+    next(err)
+})
+  
+  // error handler
+app.use(function(err, req, res, next) {
+    // set locals, only providing error in development
+    res.locals.message = err.message
+    res.locals.error = req.app.get('env') === 'development' ? err : {}
+
+    // render the error page
+    res.status(err.status || 500)
+    res.json({'message': err.message || 'Error occurred on the Server. Apologies :( '})
+})
+
+const port = process.env.PORT || 4000
+
+// sets port 400 to default or unless otherwise specified in the environment
+app.set('port', port)
 
 //Listen on port
-app.listen( ()=>{
-    console.log('Server started on Port: ' + port);
-});
+app.listen(app.get('port'), ()=>{
+    console.log('Server started on Port: ' + port)
+})
 
 
 
