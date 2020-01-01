2d293564ee7f7c805c850943504ed8473dcfc726
abhijeetjuneja <abhijeetjuneja2014@gmail.com>
2020-01-01 20:54:36 +0530

    Navbar

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,35 @@
 <template>
   <div id="app">
+    <Navigation></Navigation>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
 
+import Navigation from '@/components/Navigation'
+
 export default {
-  name: 'app'
+  name: 'app',
+  components: {
+    Navigation
+  }
 }
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   overflow-x:hidden;
+  min-height: 100vh;
   background-color: #EDEDED;
-  padding: 5vh;
+}
+@import url(http://fonts.googleapis.com/css?family=Open+Sans:300italic,400italic,700italic,300,400,700);
+body {
+    font-family: 'Open Sans', sans-serif;
 }
 .primary-title{
   color:#4214FB;
--- frontend/src/components/Navigation.vue
+++ frontend/src/components/Navigation.vue
@@ -1,7 +1,24 @@
 <template>
-  <b-row>
-    
-  </b-row>
+  <div>
+    <b-navbar toggleable="lg" type="light" variant="light">
+        <b-navbar-brand href="#"><b>HandWriting Recognition</b></b-navbar-brand>
+
+        <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+        <b-collapse id="nav-collapse" is-nav>
+
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+
+            <b-navbar-nav>
+                <b-nav-item href="/">Home</b-nav-item>
+                <b-nav-item href="/previous-results">Previous Results</b-nav-item>
+            </b-navbar-nav>
+        </b-navbar-nav>
+        </b-collapse>
+    </b-navbar>
+</div>
+
 </template>
 
 <script>
